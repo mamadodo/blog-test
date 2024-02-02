572d3118041d67294a73edec72ed85f05fcabb0d
Madoka Nagai <m.nagaii15@gmail.com>
2024-02-02 14:21:47 +0900

    文章変更

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export default function Home( { allPostsData } ) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>長井Rakです。1歳になりました。</p>
+        <p>長井Rakです。3歳になりました。</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
